c8214895477316a31402bca4d6a256cef557ea71
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-03-25 13:39:57 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button"
-
-
 export default function Home() {
   return (
-    <main> 
-      
+    <main className="flex justify-center items-center min-h-screen bg-amber-300">
+      <h1 className="text-white text-4xl font-bold">Welcome</h1>
     </main>
   );
 }
