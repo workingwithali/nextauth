cf387e3d5a51e7efea25148bc027c67326a40fee
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-04-10 19:27:08 +0500

    update

--- components/auth/Social.tsx
+++ components/auth/Social.tsx
@@ -25,7 +25,7 @@ export const Social = () => {
                     // handle GitHub login
                 }}
             >
-                <FaGithub className="w-5 h-5 mr-2" />
+                <FaGithub className="w-5 h-5 mr-1" />
                 Continue with GitHub
             </Button>
         </div>
