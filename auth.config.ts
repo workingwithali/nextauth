1859ad91d27776e1d76e1165c24a8cee7e40f7e0
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-04-15 22:45:55 +0500

    update

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,21 @@
-import GitHub from "next-auth/providers/github"
+import  Credentials  from "next-auth/providers/credentials"
 import type { NextAuthConfig } from "next-auth"
+import { LoginSchema } from "./schemas"
+import { getUserByEmail } from "@/data/user";
 
-export default { providers: [GitHub] } satisfies NextAuthConfig
+export default {
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const ValidatedFields = LoginSchema.safeParse(credentials);
+                if(ValidatedFields.success){
+                    const { email, password } =ValidatedFields.data;
+                    const user = await getUserByEmail(email);
+                    if(!email || !password) return null;
+                    const passwordMatch =await
+                }
+            },
+        })
+
+    ]
+} satisfies NextAuthConfig
