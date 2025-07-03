307530a685891c7ecbc33f360e8ac1b02b8b46f1
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-07-04 01:10:42 +0500

    update

--- auth.config.ts
+++ auth.config.ts
@@ -3,16 +3,26 @@ import type { NextAuthConfig } from "next-auth"
 import { LoginSchema } from "./schemas"
 import { getUserByEmail } from "@/data/user";
 import bcrypt from "bcryptjs";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export default {
     providers: [
+        Google({
+            clientId: process.env.GOOGLE_ID as string, 
+            clientSecret: process.env.GOOGLE_SECRET as string
+        }),
+        Github({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string
+        }),
         Credentials({
             async authorize(credentials) {
                 const ValidatedFields = LoginSchema.safeParse(credentials);
                 if(ValidatedFields.success){
                     const { email, password } =ValidatedFields.data;
                     const user = await getUserByEmail(email);
-                    if(!user || !password) return null;
+                    if(!user || !password ||!user.password) return null;
                     const passwordMatch = await bcrypt.compare(
                         password,
                         user.password,
