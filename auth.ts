d1f47eb37623a994641f604e43dccef776d08baa
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-07-03 23:14:06 +0500

    update

--- auth.ts
+++ auth.ts
@@ -7,6 +7,18 @@ import authConfig from "@/auth.config"
 
 export const { auth, handlers, signIn, signOut } = 
 NextAuth({
+    callbacks: {
+        async session({ session, user }) {
+            session.user.id = user.id;
+            return session;
+        },
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
+        },
+    }
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt" },
     ...authConfig,
