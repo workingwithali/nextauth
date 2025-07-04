d80181f23965fb2a5b9afd20f23bef702fe865f8
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-07-04 11:26:35 +0500

    update

--- components/auth/error-card.tsx
+++ components/auth/error-card.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { BsExclamationTriangle } from 'react-icons/bs'
+import { CardWrapper } from '@/components/auth/card-wrapper'
 const ErrorCard = () => {
   return (
     <div>
-      
+      <CardWrapper headerLabel="oops! something went wrong" backButtonHref="/auth/login" backButtonLabel='back to login'>
+        <div className="bg-destructive/15 p-3 rounded-md flex items-center text-sm gap-x-2 text-destructive">
+          <BsExclamationTriangle className="h-4 w-4 " />
+          <p>Something went wrong</p>
+        </div>
+      </CardWrapper>
     </div>
   )
 }
