3682be2579ee5a9779347812dd82dd5339f5d839
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-07-04 11:13:10 +0500

    update

--- auth.ts
+++ auth.ts
@@ -17,6 +17,20 @@ declare module "next-auth" {
 
 export const { auth, handlers, signIn, signOut } = 
 NextAuth({
+    pages: {
+        signIn: "/auth/login",
+        error: "/auth/login",
+    },
+    events: {
+        async linkAccount({ user }) {
+            if(user) {
+                await db.user.update({
+                    where: { id: user.id },
+                    data: { emailVerified: new Date() }
+                })
+            }
+        }
+    },
     callbacks: {
         async session({ session, token }) {
             console.log({sessiontoken: token});
