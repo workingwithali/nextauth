401eb3c44c685fa36aee3b520e7ebe0fa73bba93
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-07-04 09:54:26 +0500

    update

--- components/auth/Social.tsx
+++ components/auth/Social.tsx
@@ -2,8 +2,12 @@
 import { FcGoogle } from "react-icons/fc"
 import { FaGithub } from "react-icons/fa"
 import { Button } from "@/components/ui/button"
-
+import { signIn } from "next-auth/react"
+import { DEFAULT_LOGIN_REDIRECT } from "@/route"
 export const Social = () => {
+    const onClick = (provider: "github" | "google" ) => {
+        signIn(provider, { callbackUrl: DEFAULT_LOGIN_REDIRECT  })
+    } // onclick
     return (
         <div className="flex justify-center w-full gap-x-2">
             <Button
@@ -11,7 +15,7 @@ export const Social = () => {
                 className="flex-1"
                 variant="outline"
                 onClick={() => {
-                    // handle Google login
+                    onClick("google")
                 }}
             >
                 <FcGoogle className="w-5 h-5 mr-2" />
@@ -22,7 +26,7 @@ export const Social = () => {
                 className="flex-1"
                 variant="outline"
                 onClick={() => {
-                    // handle GitHub login
+                    onClick("github")
                 }}
             >
                 <FaGithub className="w-5 h-5 mr-2" />
