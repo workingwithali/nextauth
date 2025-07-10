16ad6674cdef1eca72d839a67db73082bd7f60d2
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-07-11 02:12:15 +0500

    update

--- components/auth/reset-form.tsx
+++ components/auth/reset-form.tsx
@@ -1,6 +1,6 @@
 "use client"
 import * as z from "zod"
-import { LoginSchema } from "@/schemas"
+import { ResetSchema} from "@/schemas"
 import { CardWrapper } from "./card-wrapper"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -26,14 +26,13 @@ export const ResetForm = () => {
   const [error,setError] = useState<string | undefined>("");
   const [success,setSuccess] = useState<string | undefined>("");
   const [isPending, startTransition] = useTransition();
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
+  const form = useForm<z.infer<typeof ResetSchema>>({
+    resolver: zodResolver(ResetSchema),
     defaultValues: {
       email: "",
-      password: "",
     },
   });
-  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+  const onSubmit = (values: z.infer<typeof ResetSchema>) => {
     setError("");
     setSuccess("");
     startTransition(() => {
