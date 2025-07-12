ea8a1109bc93781539d9700a7a0cb20486cd1093
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-07-13 01:09:27 +0500

    update

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -1,9 +1,50 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
   return (
-    <div>
-      navbar
+    <div className="bg-secondary p-4 rounded-lg shadow-md w-[600px] flex justify-between items-">
+      <div className="flex gap-x-4">
+        <Button 
+          asChild
+          variant={pathname ==="/server"?"default":"outline"}
+        >
+          <Link href="/server">
+            Server
+          </Link>
+        </Button>
+        <Button 
+          asChild
+          variant={pathname ==="/client"?"default":"outline"}
+        >
+          <Link href="/client">
+            Client
+          </Link>
+        </Button>
+        <Button 
+          asChild
+          variant={pathname ==="/Admin"?"default":"outline"}
+        >
+          <Link href="/Admin">
+            Admin
+          </Link>
+        </Button>
+        <Button 
+          asChild
+          variant={pathname ==="/setting"?"default":"outline"}
+        >
+          <Link href="/setting">
+            settings
+          </Link>
+        </Button>
+
+      </div>
+      <Button variant="outline" className="text-primary">
+        userButton
+      </Button>
     </div>
   )
 }
