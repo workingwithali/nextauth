aa41317a88b747503730abb176bdc00e596a2ff0
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-07-13 15:46:39 +0500

    update

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,8 @@
 import * as z from "zod"
 
-
+export const settingSchema = z.object({
+    name :z.optional(z.string()),
+})
 export const NewPasswordSchema = z.object({
     password: z.string().min(6, {
         message: "Password must be at least 6 characters",
