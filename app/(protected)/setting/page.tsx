63d105c21f11f9601c2d11fef62907e91a320979
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-07-14 15:35:08 +0500

    update

--- app/(protected)/setting/page.tsx
+++ app/(protected)/setting/page.tsx
@@ -1,20 +1,54 @@
 "use client";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
+import { Setting } from "@/actions/setting";
+import { useState, useTransition } from "react";
+import { settingSchema } from "@/schemas";
 import { 
   Card,
   CardContent,
   CardHeader,
  } from "@/components/ui/card";
-import { Setting } from "@/actions/setting";
-import { useTransition } from "react";
+ import { 
+  Form,
+  FormField,
+  FormItem,
+  FormControl,
+  FormLabel,
+  FormDescription,
+  FormMessage,
+ } from "@/components/ui/form";
+ import { Input } from "@/components/ui/input";
+import { useSession } from "next-auth/react";
 
 
 
 const SettingPage = () => {
+  const { update } = useSession();
   const [isPending, startTransition] = useTransition();
-  const onClick = () => {
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const form = useForm<z.infer<typeof settingSchema>>({
+    resolver: zodResolver(settingSchema),
+    defaultValues: {
+      name: "",
+    },
+  })
+  const onSubmit = (value :z.infer<typeof settingSchema>) => {
     startTransition(() => {
-      Setting( { name: "testsdsds" } );
+      Setting(value)
+      .then((res) => {
+        if(res.success){
+          update();
+          setSuccess(res.success);
+        }
+        if(res.error){
+          setError(res.error);
+        }
+      })
+      
     })
   }
   
